f562cf65e9e6aa3b3189e22eacd20c42b280e4b8
Christos Ploutarchou <cploutarchou@gmail.com>
2021-05-26 13:56:02 +0300

    update logger config based on docker-compose.yml config

--- app/logger.py
+++ app/logger.py
@@ -4,5 +4,5 @@ import graypy
 logger = logging.getLogger('AudioServer')
 logger.setLevel(logging.DEBUG)
 
-handler = graypy.GELFHTTPHandler('0.0.0.0', 12201)
+handler = graypy.GELFHTTPHandler('172.21.0.35', 12202)
 logger.addHandler(handler)
