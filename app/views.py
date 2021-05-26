f1d1abe2d3bd6c2521adb734ab57bc085b8a36d0
Christos Ploutarchou <cploutarchou@gmail.com>
2021-05-26 21:52:21 +0300

    update views

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ from logger import logger
 import models
 import plotly.express as px
 from datetime import datetime
-from flask import render_template, request, redirect, session, flash
+from flask import render_template, request, session, jsonify
 from werkzeug.utils import secure_filename
 
 from main import app
@@ -64,6 +64,7 @@ def index():
     # models.check_for_required_indexes()
     context = {
         "allowed": ', '.join(ALLOWED_EXTENSIONS),
+        "title": "AudioServer",
         "my_url": "https://christosploutarchou.com",
         "copyright": f"© 2021 All rights reserved.",
 
@@ -72,62 +73,82 @@ def index():
     if "file_urls" not in session:
         session['file_urls'] = []
     # list to hold our uploaded image urls
-    file_urls = session['file_urls']
-    failed = []
-    # handle image upload from Drop-zone
-    if request.method == 'POST':
-        if 'files[]' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        files = request.files.getlist('files[]')
-        entry_files = []
-        upload_batches = []
-        for file in files:
-            if file.filename == '':
-                flash(message='No file selected for uploading', category='error')
-                return redirect(request.url)
-            if file and allowed_file(file.filename):
-                # save the file with to our Upload folder
-                filename = secure_filename(file.filename)
-                # exists = file.get(os.path.join(app.config['UPLOADED_AUDIOS_DEST'], filename))
-                file.save(os.path.join(app.config['UPLOADED_AUDIOS_DEST'], filename))
-                os.path.getsize(app.config['UPLOADED_AUDIOS_DEST'] + "/" + filename)
-                size = os.path.getsize(app.config['UPLOADED_AUDIOS_DEST'] + "/" + filename)
-                name, type = filename.split('.')
-                data = {
-                    "format_type": type,
-                    'title': name,
-                    'file_size': size,
-                    "status": 1,
-                    "updated_at": datetime.now()
-                }
-                object_id = models.insert_entry(data)
 
-                if object_id is not None:
-                    entry_files.append(str(object_id))
-                    logger.info(f"File {filename} successfully uploaded.")
-                else:
-                    failed.append(filename)
-
-                flash('File(s) successfully uploaded')
-            else:
-                flash(f"Allowed file types are {context['allowed']}")
-                return redirect(request.url)
-                # append image url
-            file_urls.append(filename)
-        if len(entry_files) > 0:
-            inserted_batches = models.insert_batch(entry_files)
-            if len(inserted_batches) > 0:
-                res = models.insert_upload(inserted_batches)
-                context['uploaded_id'] = res
-        return render_template('index.html', data=context)
     # return dropzone template on GET request
     return render_template('index.html', data=context)
+
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    return_response = None
+    job_uuid = None
+    if request.method == 'POST':
+        file = None
+        if request.files and "file" in request.files:
+            file = request.files['file']
+        else:
+            return_response = jsonify(dict(code=400, msg="No Files"))
+        if "uuid" in request.form:
+            job_uuid = request.form['uuid']
+        if job_uuid is None:
+            return_response = jsonify(dict(code=400, msg="Something going wrong."))
+        if file.filename == '':
+            return_response = jsonify(dict(code=400, msg="No valid file"))
+        if file and allowed_file(file.filename):
+            # save the file with to our Upload folder
+            filename = secure_filename(file.filename)
+            # exists = file.get(os.path.join(app.config['UPLOADED_AUDIOS_DEST'], filename))
+            file.save(os.path.join(app.config['UPLOADED_AUDIOS_DEST'], filename))
+            os.path.getsize(app.config['UPLOADED_AUDIOS_DEST'] + "/" + filename)
+            size = os.path.getsize(app.config['UPLOADED_AUDIOS_DEST'] + "/" + filename)
+            name, format_type = filename.split('.')
+            data = {
+                "format_type": format_type,
+                'title': name,
+                'file_size': size,
+                "status": 1,
+                "updated_at": datetime.now(),
+                "job_id": str(job_uuid)
+            }
+            object_id = models.insert_entry(data)
+
+            if object_id is not None:
+                return_response = jsonify(dict(code=200, msg="File successive uploaded"))
+
+        else:
+            allowed = ', '.join(ALLOWED_EXTENSIONS)
+            return_response = jsonify(dict(code=400, msg=f"Allowed extensions file : {allowed}"))
+    return return_response
+
+
+@app.route("/create_batch", methods=['POST'])
+def create_batch():
+    job_uuid = None
+    return_response = None
+    if request.method == 'POST':
+        if "uuid" in request.form:
+            job_uuid = request.form['uuid']
+        if job_uuid is not None:
+            entry_files = models.Files.objects(job_id=job_uuid)
+            if len(entry_files) == 0:
+                return_response = jsonify(dict(code=201, msg="No files found"))
+            files = []
+            for file in entry_files:
+                files.append(str(file.id))
+            if len(files) > 0:
+                inserted_batches = models.insert_batch(files)
+                if inserted_batches and len(inserted_batches) > 0:
+                    res = models.insert_upload(inserted_batches)
+                    if res is not None:
+                        return_response = jsonify(
+                            dict(code=200, msg="File upload completed successfully", upload_id=str(res)))
+    return return_response
 
 
 @app.route("/stats")
 def stats():
     context = {"top_10": models.get_top_10(),
+               "title": "AudioServer",
                "average_file_size": avg()['AverageValue'],
                "total_files": models.Files.objects().count(),
                "total_size": models.convert_size(models.Files.objects.sum('file_size'))
