87d2baddefd700da6abdd6547742adaf80439b02
Christos Ploutarchou <cploutarchou@gmail.com>
2021-05-29 03:52:17 +0300

    create login, register and verify routes

--- app/views.py
+++ app/views.py
@@ -1,50 +1,50 @@
 import errno
-import glob
 import os
 import json
-from uuid import uuid4
+from secrets import token_hex
 
 import flask
-import mongoengine
+
 import pandas as pd
 import plotly
 from mongoengine import errors
-from mongoengine.errors import ValidationError, LookUpError, SaveConditionError
+from mongoengine.errors import ValidationError, SaveConditionError
 
 from logger import logger
 import models
 import plotly.express as px
 from datetime import datetime
-from flask import render_template, request, session, jsonify, url_for, send_from_directory
+from flask import render_template, request, session, jsonify, url_for, send_from_directory, flash
 from werkzeug.utils import secure_filename, redirect
+import bcrypt
 
 from main import app
 
 uploads_dir = app.config['UPLOADED_AUDIOS_DEST']
 os.makedirs(uploads_dir, exist_ok=True)
 
-ALLOWED_EXTENSIONS = set(['mp3', 'wav', 'ogg'])
+ALLOWED_EXTENSIONS = {'mp3', 'wav', 'ogg'}
 
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/find/<id>', methods=['GET'])
-def find(id=None):
-    if id is None:
+@app.route('/find/<_id>', methods=['GET'])
+def find(_id=None):
+    if _id is None:
         return {
             'status_code': 400,
             'status_msg': 'Bad Request',
             'description': "Post id is required."
         }
-    return models.get_upload_details(id)
+    return models.get_upload_details(_id)
 
 
-@app.route('/file/<id>', methods=['GET'])
-def get_file(id):
-    print(id)
-    return models.get_file(id)
+@app.route('/file/<_id>', methods=['GET'])
+def get_file(_id):
+    print(_id)
+    return models.get_file(_id)
 
 
 @app.route('/top_10', methods=['GET'])
@@ -62,8 +62,79 @@ def last_7_days_upload():
     return models.last_7_days_upload()
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        users = models.Users
+        login_user = users.objects(username=request.form['username']).first()
+        if login_user and login_user['status'] == 1:
+            if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password'].encode('utf-8')) == \
+                    login_user['password'].encode('utf-8'):
+                session['username'] = request.form['username']
+                session['verified'] = True
+                return redirect(url_for('index'))
+
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    required_fields = ['username', 'password', "confirm_password", "email"]
+    if request.method == 'POST':
+        if all(field in request.form for field in required_fields):
+            if request.form['password'] == request.form['confirm_password']:
+                print("OK")
+                existing_user = models.Users.objects(username=request.form['username'],
+                                                     password=request.form['password'])
+                print(existing_user)
+                if len(existing_user) == 0:
+                    hash_pass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+                    try:
+                        user = models.Users(username=request.form['username'], password=hash_pass,
+                                            email=request.form['email'], verification_token=token_hex(25)).save()
+                        print(user.verification_token)
+                        if user and user.id:
+                            session['username'] = request.form['username']
+                    except SaveConditionError as e:
+                        flash(f"Something going wrong. Unable to register . Error : {e}", category="error")
+                        return render_template('register.html')
+                    flash(
+                        f"Your account has been successfully created. Please verify your account before continue. "
+                        f"Verification mail has been sent to your email address",
+                        category="success")
+                    return redirect(url_for('index'))
+                else:
+                    flash(f"That username already exists!")
+                    return render_template('register.html')
+            else:
+                flash("Password is not match", category="error")
+                return render_template('register.html')
+        else:
+            flash("Pleaser fill all form fields", category="error")
+            return redirect(url_for('register'))
+    return render_template('register.html')
+
+
+@app.route('/verify', methods=['GET'])
+def verify():
+    # Sample Url:
+    # http://127.0.0.1:5000/verify?id=60b17819dbf1d5c899833a11&token=49c97c2abba5476ddcde29cac86f3266df75d1e5746fa434b1
+    _ = request.args.get('id')
+    token = request.args.get('token')
+    if _ is not None and token is not None:
+        users = models.Users
+        login_user = users.objects(id=_).first()
+        if str(token) == str(login_user['verification_token']):
+            data = {"status": 1, "updated_at": datetime.now()}
+            login_user.update(**data)
+            flash("Your account has been successfully verified. Please log in", category="success")
+            return render_template('login.html')
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if 'username' not in session or session.get('username') == "" or "verified" not in session:
+        return redirect(url_for('login'))
     # redis.incr('hits')
     # models.check_for_required_indexes()
     context = {
@@ -132,12 +203,12 @@ def stats():
 
     fig.update_layout(barmode='stack')
     fig.update_xaxes(categoryorder='category ascending')
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    graph_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     header = "Weekly Uploads"
     description = """
 
         """
-    return render_template('stats.html', data=context, graphJSON=graphJSON, header=header, description=description)
+    return render_template('stats.html', data=context, graphJSON=graph_json, header=header, description=description)
 
 
 @app.errorhandler(code_or_exception=404)
@@ -197,6 +268,7 @@ def uploads():
                 try:
                     models.insert_entry(data)
                 except errors.SaveConditionError as e:
+                    logger.error(f"Something went wrong. Error : {str(e)}")
                     failed_files.append(file)
 
             else:
