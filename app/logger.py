f5a01c8f8c30f9497e6277a71d508f5a3ceaa461
Christos Ploutarchou <cploutarchou@gmail.com>
2021-05-29 23:43:17 +0300

    Correction

--- app/logger.py
+++ app/logger.py
@@ -4,5 +4,5 @@ import graypy
 logger = logging.getLogger('AudioServer')
 logger.setLevel(logging.DEBUG)
 
-handler = graypy.GELFHTTPHandler('172.21.0.35', 12202)
+handler = graypy.GELFHTTPHandler('172.21.0.35', 12201)
 logger.addHandler(handler)
