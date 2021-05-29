a5c3556c41f7a23a9e9f4426342b4bd5f15a07b2
Christos Ploutarchou <cploutarchou@gmail.com>
2021-05-29 03:52:30 +0300

    correction

--- app/static/js/upload.js
+++ app/static/js/upload.js
@@ -1,6 +1,20 @@
-/******************************************************************************
- * HTML5 Multiple File Uploader Demo                                          *
- ******************************************************************************/
+// window.addEventListener('DOMContentLoaded', event => {
+//
+//     // Toggle the side navigation
+//     const sidebarToggle = document.body.querySelector('#sidebarToggle');
+//     if (sidebarToggle) {
+//         // Uncomment Below to persist sidebar toggle between refreshes
+//         // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+//         //     document.body.classList.toggle('sb-sidenav-toggled');
+//         // }
+//         sidebarToggle.addEventListener('click', event => {
+//             event.preventDefault();
+//             document.body.classList.toggle('sb-sidenav-toggled');
+//             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+//         });
+//     }
+//
+// });
 let generateUUID = () => {
     let d = new Date().getTime();//Timestamp#}
     let d2 = (performance && performance.now && (performance.now() * 1000)) || 0;//Time in microseconds since page-load or 0 if unsupported#}
