f05299b9e7c509779f5d800b5c046b1dd542a55b
Christos Ploutarchou <cploutarchou@gmail.com>
2021-05-29 18:21:59 +0300

    corrections

--- run.sh
+++ run.sh
@@ -1,2 +1,22 @@
+#!/bin/bash
+echo "###################################################"
+echo "#      Start building application image           #"
+echo "###################################################"
 docker-compose build
+echo "###################################################"
+echo "#    Application image successfully CREATED.      #"
+echo "###################################################"
+echo "###################################################"
+echo "## Trying to start docker compose infrastructure. #"
+echo "###################################################"
 docker-compose up -d
+echo "###################################################"
+echo "#     Infrastructure successfully started.        #"
+echo "###################################################"
+echo "###################################################"
+echo "##       Start mongodb initialization             #"
+echo "###################################################"
+./scripts/init.sh
+echo "###################################################"
+echo "#    INFRASTRUCTURE SUCCESSFULLY STARTED          #"
+echo "###################################################"
