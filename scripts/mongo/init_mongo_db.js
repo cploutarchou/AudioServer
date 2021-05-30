e1b25ee0d1b7bcbd73c452ede8193580fa463ed8
Christos Ploutarchou <cploutarchou@gmail.com>
2021-05-31 00:58:05 +0300

    corrections

--- scripts/mongo/init_mongo_db.js
+++
@@ -1,8 +0,0 @@
-db.createCollection("Batches", {
-    capped: Boolean(),
-    autoIndexId: Boolean(),
-    name: String(),
-    created_at: ISODate(),
-    updated_at: ISODate(),
-    files: [],
-});
