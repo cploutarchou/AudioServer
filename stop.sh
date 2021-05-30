5aa0533b0180e393313e4d7be0a86eb0e25ea00e
Christos Ploutarchou <cploutarchou@gmail.com>
2021-05-30 04:03:49 +0300

    Corrections

--- stop.sh
+++ stop.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+echo "###################################################################"
+echo "#       Trying to Stop AUDIO SERVER INFRASTRUCTRURE               #"
+echo "###################################################################"
 docker-compose down
+echo "####################################################################"
+echo "#    AUDIO SERVER INFRASTRUCTRURE HAS BEST SUCCESSFULLY STOPPED    #"
+echo "####################################################################"
