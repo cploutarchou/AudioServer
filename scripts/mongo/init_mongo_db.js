102ddae3dc21a15e243fda876e1f23edd072ac7d
Christos Ploutarchou <cploutarchou@gmail.com>
2021-05-30 04:06:41 +0300

    Refactoring

--- scripts/mongo/init_mongo_db.js
+++ scripts/mongo/init_mongo_db.js
@@ -1,10 +1,8 @@
-db.createCollection("Batches",
-    {
-        capped: Boolean(),
-        autoIndexId: Boolean(),
-        name: String(),
-        created_at: ISODate(),
-        updated_at: ISODate(),
-        files: [],
-    }
-);
+db.createCollection("Batches", {
+    capped: Boolean(),
+    autoIndexId: Boolean(),
+    name: String(),
+    created_at: ISODate(),
+    updated_at: ISODate(),
+    files: [],
+});
